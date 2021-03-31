551398002c9b2c0365409fb240a1127d685db8d0
Matias <mtroncoso@stratechcorp.com>
2021-03-30 23:41:51 -0300

    v2 stable

--- index.js
+++ index.js
@@ -1,74 +1,76 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const {
-    Client
-} = require('pg')
+const { Client } = require("pg");
 
-const PORT = process.env.PORT || 8080
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8888;
 
 const client = new Client({
-    host: 'db',
-    user: 'admin',
-    password: '1234',
-    database: 'db',
+  host: "db",
+  user: "admin",
+  password: "1234",
+  database: "db",
 });
 
-client.connect()
+client.connect();
 
-app.post('/data', async (req, res) => {
-
-    try {
-        await client
-            .query(`CREATE TABLE IF NOT EXISTS users (
+app.post("/data", async (req, res) => {
+  const datosA = req.body.datos;
+  console.log(datosA);
+  try {
+    await client
+      .query(
+        `CREATE TABLE IF NOT EXISTS users (
         nombre VARCHAR(100) ,
         apellido VARCHAR(100) 
-        );`)
-            .then(res => console.error("CREADA"))
-            .catch(e => console.error("NO CREADA"))
+        );`
+      )
+      .then((res) => console.error("CREADA"))
+      .catch((e) => console.error("NO CREADA"));
 
-        await datos();
+    // await datos();
 
-        console.log(await selectData())
-    } catch (error) {
-        console.log(error)
-    }
+    // console.log(await selectData());
+  } catch (error) {
+    console.log(error);
+  }
 
-    res.status(200).json("creado")
-    // client.end()
-})
+  res.status(200).json("creado");
+  // client.end()
+});
 
 const datos = async () => {
+  const query = {
+    // give the query a unique name
+    name: "fetch-user",
+    text: "INSERT INTO users(nombre,apellido) VALUES ($1,$2)",
+    values: ["Mario", "Guitierrez"],
+  };
 
-    const query = {
-        // give the query a unique name
-        name: 'fetch-user',
-        text: 'INSERT INTO users(nombre,apellido) VALUES ($1,$2)',
-        values: ['Mario', 'Guitierrez'],
+  // callback
+  client.query(query, (err, res) => {
+    if (err) {
+      console.log("ERROR", err);
+    } else {
+      console.log("INSERTADO");
     }
-
-    // callback
-    client.query(query, (err, res) => {
-        if (err) {
-            console.log("ERROR", err)
-        } else {
-            console.log("INSERTADO")
-        }
-    })
-}
+  });
+};
 
 const selectData = async () => {
-
-    // callback
-    await client.query('SELECT * FROM users', (err, res) => {
-        if (err) {
-            console.log(err.stack)
-        } else {
-            console.log(res.rows)
-        }
-    })
-}
+  // callback
+  await client.query("SELECT * FROM users", (err, res) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      console.log(res.rows);
+    }
+  });
+};
 
 app.listen(PORT, () => {
-    console.log("Server on port ", PORT)
-})
+  console.log("Server on port ", PORT);
+});
